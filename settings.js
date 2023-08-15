35f8faf503bb6e530e3a45bc0785f726fad43d0a
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-08-15 07:09:54 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,11 +17,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.BOT_TOKEN = "6434935849:AAHxvwWhM8cL4tRzawFBwOnjI54NtYdnjPo" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "🤖JengkolOnline🤖" //your bot name
+global.OWNER_NAME = "Jengkol_Online" //your name
+global.OWNER_NUMBER = "6282372139631" //your telegram number
+global.OWNER = ["https://t.me/Jengkol_Online", "https://t.me/Jengkol_Online"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
